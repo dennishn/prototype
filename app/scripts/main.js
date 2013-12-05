67fc8bd5a569aebc61d6ca2f8be23c493e49cedc
Dennis Haulund Nielsen <dhni@nodes.dk>
2013-12-05 23:42:29 +0100

    Type Test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
     $(document).foundation();
+
+    //Prototype variables
+    var windowWidth 	= $(window).outerWidth(),
+    	windowHeight	= $(window).outerHeight();
+    
+    //Article Gallery Plugin
+    var $gallery 		= $('.supplementary > .gallery'),
+    	$gallerySection = $gallery.find('.section');
+    var gallerySections = $gallery.find('.section').length;
+    $gallery.css({
+    	'width': windowWidth * gallerySections
+    });
+    $gallerySection.each(function(i, el) {
+    	$(el).css({
+    		'width': windowWidth
+    	});
+    });
 });
