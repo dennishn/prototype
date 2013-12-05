046500101b7c95fc45b8f19b6a23aaba903eb4cd
Dennis Haulund Nielsen <dhni@nodes.dk>
2013-12-05 07:37:05 +0100

    Type Test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,3 @@
 $(document).ready(function() {
     $(document).foundation();
-
-    $(document).offcanvas({
-        'onOpen': function(){
-            console.log('Opening');
-        },
-        'onOpened': function(){
-            console.log('Opened');
-        },
-        'onClose': function(){
-            console.log('Closing');
-        },
-        'onClosed': function(){
-            console.log('Closed');
-        }
-    });
 });
